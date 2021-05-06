c691d0d9324469760b476c32706ebca76c193f1e
Nelson Alves de Oliveira <nelsonalves.olv@gmail.com>
2021-05-06 15:37:39 -0300

    criação microsserviços consumer e producer

---
+++ poc-rabbitmq-consumer/src/main/java/br/com/sankhya/PocRabbitmqConsumerApplication.java
@@ -0,0 +1,13 @@
+package br.com.sankhya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocRabbitmqConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocRabbitmqConsumerApplication.class, args);
+	}
+
+}
---
+++ poc-rabbitmq-producer/src/main/java/br/com/sankhya/PocRabbitmqProducerApplication.java
@@ -0,0 +1,13 @@
+package br.com.sankhya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocRabbitmqProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocRabbitmqProducerApplication.class, args);
+	}
+
+}
